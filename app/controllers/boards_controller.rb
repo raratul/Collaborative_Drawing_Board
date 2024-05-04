8e6b442836738f1bebc25d202fb1a5e644b0b051
raratul <rkhairulislam@gmail.com>
2024-05-04 20:39:38 +0600

    updates for deployment...

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,4 +1,50 @@
+# class BoardsController < ApplicationController
+#   def index
+#     @boards = Board.all
+#     @user = User.new
+#   end
+
+#   # def create
+#   #   if params[:nickname].blank? 
+#   #     flash[:error] = "Nickname cannot be blank"
+#   #     redirect_to root_path
+#   #   else
+#   #     user = User.create(nickname: params[:nickname])
+#   #     if user.valid?
+#   #       board = user.boards.create(name: "Default Board")
+#   #     else
+#   #       flash[:error] = "Failed to create user"
+#   #       redirect_to root_path
+#   #     end
+#   #   end
+#   # end
+
+#   def create
+#     if params[:nickname].blank? 
+#       flash[:error] = "Nickname cannot be blank"
+#       redirect_to root_path
+#     else
+#       user = User.create(nickname: params[:nickname])
+#       if user.valid?
+#         board = user.boards.create(name: "Default Board")
+#         redirect_to board # Redirect to the show action for the newly created board
+#       else
+#         flash[:error] = "Failed to create user"
+#         redirect_to root_path
+#       end
+#     end
+#   end
+  
+
+#   def show
+#     @board = Board.find(params[:id])
+#   end
+# end
+
+# app/controllers/boards_controller.rb
 class BoardsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :board_not_found
+
   def index
     @boards = Board.all
     @user = User.new
@@ -12,6 +58,7 @@ class BoardsController < ApplicationController
       user = User.create(nickname: params[:nickname])
       if user.valid?
         board = user.boards.create(name: "Default Board")
+        redirect_to board
       else
         flash[:error] = "Failed to create user"
         redirect_to root_path
@@ -21,5 +68,12 @@ class BoardsController < ApplicationController
 
   def show
     @board = Board.find(params[:id])
+  end
+
+  private
+
+  def board_not_found
+    flash[:alert] = "Board not found"
+    redirect_to boards_path
   end
 end
