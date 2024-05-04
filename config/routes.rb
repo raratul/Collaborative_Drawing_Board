4c9428e19f1e55160f79fc5edeba525724a1a866
raratul <rkhairulislam@gmail.com>
2024-05-04 21:16:02 +0600

    updates for deployment..

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # mount ActionCable.server => '/cable'
   root 'boards#index'
   resources :boards
-  # post '/boards/create', to: 'boards#create', as: 'boards_create'
 end
