27f5634f69facbddd9a7db32f13144a538090c73
raratul <rkhairulislam@gmail.com>
2024-05-05 12:28:04 +0600

    updated render build..

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,11 +4,7 @@ set -o errexit
 
 chmod +x bin/rails
 
-bundle config set force_ruby_platform true
 bundle install
-bundle exec rake assets:precompile
-bundle exec rake assets:clean
-bundle exec rake db:migrate
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails
